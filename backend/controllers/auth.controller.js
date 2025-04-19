9713d311a47b0a68d9fe8f090904951caa25fdc5
Vigo Luthjens <2101466luthjens@zuyd.nl>
2025-04-19 12:00:03 +0200

    Auth functions

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,6 @@
+import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
     try {
@@ -15,20 +17,29 @@ export const signup = async (req, res) => {
         }
 
         //Hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         const girlsProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
-        
+
         const newUser = new User({
             fullName,
             username,
-            password,
+            password: hashedPassword,
             gender,
             profilePicture: gender === "male" ? boyProfilePic : girlsProfilePic,
         });
 
-        await newUser.save();
-        res.status(201).json({ message: "User created successfully" });
+        if (newUser) {
+            generateTokenAndSetCookie(newUser._id, res);
+            await newUser.save();
+            res.status(201).json({ message: "User created successfully" });
+        }
+        else {
+            res.status(400).json({ error: "User creation failed" });
+        }
+
     } catch (error) {
         console.error("Error creating user:", error.message);
         res.status(500).json({ error: "Internal server error" });
@@ -36,9 +47,39 @@ export const signup = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-    console.log("Login user");
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+        if (!user || !isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid username or password" });
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePicture: user.profilePicture,
+        });
+
+    } catch (error) {
+        console.error("Error user login:", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
 }
 
-export const logout = async (req, res) => {
-    console.log("Logout user");
+export const logout = (req, res) => {
+    try {
+        res.cookie("jwt", "", {
+            maxAge: 0,
+        });
+        res.status(200).json({ message: "User logged out successfully" });
+
+    } catch (error) {
+        console.error("Error user logout:", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
 }
